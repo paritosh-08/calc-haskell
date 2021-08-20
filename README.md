db1afbc3f2874d3f77a7fc6ddd50803fb014437f
Paritosh Raj <paritosh@hasura.io>
2021-08-21 02:38:50 +0530

    Basic parsing using megaparsec

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # calc-haskell
+An awesome (?) calculator in Haskell.
+
+## What can I do with this?
+You can do a lot of things with this piece of code:
+* Show someone the worst way to parse an expression.
+* Delete the code and save some space for gifs like [this](https://i.imgur.com/hbgtV4y.gif).
+* Do some simple calculations (if you have a lot of time).
+
+## I want to do some calculations
+If you really want to do calculations then please follow the following:
+
+First, install and run the `cabal` project:
+``` sh
+git clone https://github.com/paritosh-08/calc-haskell.git
+cd calc-haskell
+cabal new-run
+```
+
+Now enter the expression that needs to be evaluated.
+
+### Expression Docs
+The expression is of three types:
+* Number: This is an INTEGER (only as of now) and you need to directly enter the number, eg `5` is an expression but `5.0` will give an error
+* Function: There are two functions as of now:
+    * `sqrt`: This takes an `expression` and returns the square root of the evaluted `expression`. i.e. I can write `sqrt(4)` and this will be evauated to `2`.
+    * `ifzero`: This function has the syntax: `ifzero(<expression1> ? <expression2> : <expression3>)`. `<expression1>` is the condition that will be evaluated and checked if it is zero or not. If `<expression1>` turns out to be zero then, the function will return `<expression2>`, else it will return `<expression3>`.
+* Binary Operations: This will always be enclosed within brackets, i.e the syntax is `(<expression1> <OPERATOR> <expression2>)`. Here `<OPERATOR>` can be anything from the list: [`+`,`-`,`/`,`*`,`^`]. The meaning of each operator is defined below:
+    | Operator | Meaning                                        | Example | Result |
+    |:--------:|------------------------------------------------|:-------:|:------:|
+    | +        | Adds two entities                              | 2+2     | 4      |
+    | -        | Subtracts second entity from the first one     | 3-2     | 1      |
+    | *        | Multiplies two entities                        | 3*2     | 6      |
+    | /        | Divides first entity by the second one         | 6/3     | 2      |
+    | ^        | Raises first entity by the power of second one | 2^3     | 8      |
+
+### Examples of expressions
+| Expression    | Result    | Explanation (if any)                                          |
+|---------------|:---------:|---------------------------------------------------------------|
+| 45            | 45        | Integers can act as an expression                             |
+| (2+5)         | 7         | This is an example of *Binary Operation*                      |
+| ((3+5)/4)     | 2         | Binary operation where first argument is a *Binary operation* |
+| ifzero(0?5:6) | 5         | An example of *Function Expression*                           |
+| sqrt(16)      | 4         | Another example of *Function Expression*                      |
+
+### Errors in expression evaluation
+Sometimes the programme will return `Something went wrong`. The error handling will be done in future, but for now, you can debug by the following steps:
+* First run `cabal repl`.
+* Now write `parseTest parseExpression <EXPRESSION>` and hit enter (replace `<EXPRESSION>` by the expression which caused some error).
+* Hopefully, you will get some human readable error.
+* To exit, just write `:q` and hit enter.
+
+## Things to keep in Mind
+> Please use integers only in the expression.
+
+> Always use brackets to envelope the operator expression, `2+2` will give error for sure. Instead write `(2+2)`.
--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad.Except
+import Data.Text (Text, pack)
+import Data.Void
+import Control.Applicative hiding (many, some)
+import Control.Monad.IO.Class
 import Text.Megaparsec
-import Control.Applicative
+import qualified Text.Megaparsec.Char as M
+import qualified Text.Megaparsec.Char.Lexer as L
 
 type CalcMonad = ExceptT CalcException IO Double
 
-data CalcException = NegativeSQRTError | DivideByZeroError deriving (Show)
+data CalcException = NegativeSQRTError | DivideByZeroError | IncompatibleExp deriving (Show)
 
 addF :: Double -> Double -> CalcMonad
 addF a b = return (a + b)
@@ -31,14 +38,17 @@ ifZero :: Double -> Double -> Double -> CalcMonad
 ifZero 0 x _ = return x
 ifZero _ _ y = return y
 
-data Op = Add | Sub | Div deriving (Show)
+powF :: Double -> Double -> CalcMonad
+powF x y = return $ x**y
+
+data Op = Add | Sub | Div | Mult | Pow | Err deriving (Show)
 
 data Expression = 
     Number Double
   | Operator Op Expression Expression -- binary
   | SQR Expression
   | IfZ Expression Expression Expression
-  | Mult Expression Expression
+  --- | Mult Expression Expression
   deriving (Show)
 
 evaluate :: Expression -> CalcMonad
@@ -50,18 +60,30 @@ evaluate (Operator op x y) = do
         Add -> addF
         Sub -> subtractF
         Div -> divideF
+        Pow -> powF
+        Mult -> (\p _ -> do
+          y'' <- catchError (evaluate y) (\e -> case e of
+            DivideByZeroError -> if x' == 0 
+              then do
+                liftIO $ print "Error"
+                return 0 
+              else throwError e
+            _ -> throwError e)
+          multiplyF p y''
+          )
+        Err -> (\_ _ -> throwError IncompatibleExp)
   oper x' y'
 
-evaluate (Mult x y) = do
+{- evaluate (Mult x y) = do
   x' <- evaluate x
   y' <- catchError (evaluate y) (\e -> case e of
-    NegativeSQRTError -> throwError e
     DivideByZeroError -> if x' == 0 
       then do
         liftIO $ print "Error"
         return 0 
-      else throwError e )
-  multiplyF x' y'
+      else throwError e
+    _ -> throwError e)
+  multiplyF x' y' -}
   
 evaluate (SQR x) = do
   x' <- evaluate x
@@ -73,10 +95,98 @@ evaluate (IfZ x y z) = do
   ifZero x' y' z'
 
 myEXP = 
-  Operator Add (Number 5) (Operator Sub (Mult (Number 0) (Operator Div (Operator Add (Number 1) (Number 7)) (Number 0))) (Number 2))
+  Operator Add (Number 5) (Operator Sub (Operator Mult (Number 0) (Operator Div (Operator Add (Number 1) (Number 7)) (Number 0))) (Number 2))
+
+-------------------------------------------------------------------
+
+type Parser a = Parsec Void String a
+
+spaceConsumer :: Parser ()
+spaceConsumer = M.space
+
+parseOp :: Parser Op
+parseOp = do
+  a <- oneOf ['+','-','/','*','^']
+  case a of
+    '+' -> return Add
+    '-' -> return Sub
+    '/' -> return Div
+    '*' -> return Mult
+    '^' -> return Pow
+    _ -> return Err
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme spaceConsumer
+
+symbol :: String -> Parser String
+symbol = L.symbol spaceConsumer
+
+signedDouble :: Parser Double
+signedDouble = L.signed spaceConsumer float
+  where
+    float = lexeme L.decimal
+
+floatingNum :: Parser Double
+floatingNum = L.float
+
+parseNumber :: Parser Expression
+parseNumber = Number <$> signedDouble
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+parseExpression :: Parser Expression
+parseExpression = 
+      parseNumber
+  <|> parseExp
+  <|> parseSQRT
+  <|> parseIFz
+
+parseExp :: Parser Expression
+parseExp = do
+  _ <- symbol "("
+  x <- parseNumber <|> parseExpression
+  op <- parseOp <* spaceConsumer
+  y <- parseNumber <|> parseExpression
+  _ <- symbol ")"
+  pure (Operator op x y)
+
+parseSimExp :: Parser Expression
+parseSimExp = do
+  x <- parseNumber <* spaceConsumer
+  op <- parseOp <* spaceConsumer
+  y <- parseNumber <* spaceConsumer
+  pure (Operator op x y)
+
+
+parseSQRT :: Parser Expression
+parseSQRT = do
+  t <- chunk "sqrt"
+  _ <- symbol "("
+  x <- parseNumber <|> parseExp
+  _ <- symbol ")"
+  pure (SQR x)
+
+parseIFz :: Parser Expression
+parseIFz = do
+  _ <- chunk "ifzero"
+  _ <- symbol "("
+  x <- parseNumber <|> parseExp
+  _ <- symbol "?"
+  y <- parseNumber <|> parseExp
+  _ <- symbol ":"
+  z <- parseNumber <|> parseExp
+  _ <- symbol ")"
+  pure (IfZ x y z)
+
 
 main :: IO ()
 main = do
-  print myEXP
-  t <- runExceptT $ evaluate myEXP
-  print t
+  putStrLn "Enter an expression:"
+  e <- getLine
+  let expr = parseMaybe parseExpression e
+  case expr of 
+    Nothing -> putStrLn "Something went wrong"
+    Just expr' -> do
+      res <- runExceptT (evaluate expr')
+      print res
